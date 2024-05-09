134e2c20c1375ee1b856ca36c511e61de25e4405
SoraHjort <callmehjort@gmail.com>
2024-05-09 12:05:36 +0000

    Update PF2eCampaignNotesToDialogWindow.js
    additional cleanup

--- FoundryVTT-Macros/PF2eCampaignNotesToDialogWindow.js
+++ FoundryVTT-Macros/PF2eCampaignNotesToDialogWindow.js
@@ -13,52 +13,52 @@
 //lines over the line limit, soooo.... yeah.
 
 //Get all selected tokens
-selectedActors = canvas.tokens.controlled;
+let selectedActors = canvas.tokens.controlled;
 
 //setup a array to throw in compares later
-dupeCheck = [];
+let dupeCheck = [];
 
 //minimum dimensions for dialog
-minWidth = 200; 
-minHeight = 200; //<=== is for spacing calculations only
+const minWidth = 200; 
+const minHeight = 200; //<=== is for spacing calculations only
 
 //alias for the viewport's dimensions
-winWidth = window.innerWidth;
-winHeight = window.innerHeight;
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
 
 //max dimensions
-maxWidth = winWidth/2;
-maxHeight = winHeight*0.9;
+let maxWidth = winWidth/2;
+let maxHeight = winHeight*0.9;
 
 //dirty max distance psuedo margin calculation
-maxX = winWidth - 200;
-maxY = winHeight;
+let maxX = winWidth - 200;
+let maxY = winHeight - 0;
 
 //get the number of min dimensiosn can fit in viewport
-widthCount = Math.floor(maxX / minWidth);
-heightCount = Math.floor(maxY / minHeight) + 1;
+let widthCount = Math.floor(maxX / minWidth);
+let heightCount = Math.floor(maxY / minHeight) + 1;
 
 //loop through every selected token
 for (let key in selectedActors) {
     
     //alias to the current loop's token
-    selectedActor = canvas.tokens.controlled[key].actor;
+    let selectedActor = canvas.tokens.controlled[key].actor;
     
     //double check whether or not Campaign Notes exists on the token as 
     //they don't exist on NPC or Familiar sheets
     try {
         
         //alias campaign notes
-        campaignNotes = selectedActor.system.details.biography.campaignNotes
+        var campaignNotes = selectedActor.system.details.biography.campaignNotes
         
         } catch(e) {
         //if not exist, set the value to a empty string
-        campaignNotes = "";
+        var campaignNotes = "";
         }
 
     //Alias the sheet name and image
-    charName = selectedActor.name;
-    charImg = selectedActor.img;
+    let charName = selectedActor.name;
+    let charImg = selectedActor.img;
     
     //If campaign notes aren't empty, and also don't already exist within 
     //the dupeCheck Array, then proceed.
@@ -69,17 +69,19 @@ for (let key in selectedActors) {
         
         //Setup positional data for the dialog box(es)
         //check the number of entries in dupeCheck
-        i = dupeCheck.length - 1;
+        let i = dupeCheck.length - 1;
         
         //Modulo calculations on how many can fit left to right, top to 
         //bottom.
-        x = (i % widthCount) + 1;
-        y = (Math.floor(i/widthCount) % heightCount) + 1;
+        let x = (i % widthCount) + 1;
+        let y = (Math.floor(i/widthCount) % heightCount) + 1;
         
         //Save positional data calculation
-        dialogBoxOptions = {
+        let dialogBoxOptions = {
             top: y * 150 - 100,
-            left: x * 200 - 50 }
+            left: x * 200 - 50, 
+            classes: ["dialog", "campaignNotesDialogGGP"]
+            }
             
             
         //Setup template for the html within the dialog window. Due to 
@@ -104,44 +106,53 @@ for (let key in selectedActors) {
         //Then we insert the campaign notes before closing up the div.
         
         //And because campaign notes are stored as HTML, it'll render in the dialog window much like it does on the sheet.
-        dialogBox = `
-        <style>
-        .dialog:has(.window-content .dialog-content .campaignNotesDialog ) {
-            background-color: rgba(0,0,0,0.5);
-            background-blend-mode: lighten;
-            min-width: `+minWidth+`px !important;
-            max-width: `+maxWidth+`px !important;
-            max-height:`+maxHeight+`px !important;
-            overflow-x: auto;
-            overflow-y: scroll;
-            width: auto !important;
-            backdrop-filter: blur(2px);
-            }
-        .dialog:has(.window-content .dialog-content .campaignNotesDialog ) > .window-header {
-            background: rgba(0,0,0,0.25);
-            font-size: 16pt;
-            height: auto;
-            }
-        .window-content:has(.dialog-content .campaignNotesDialog ) {
-            background: unset !important;
-            }
-        .campaignNotesDialog {
-            color:white;
-            }
-        #charImgFor`+charName+` {
-            max-width: 50px;
-            max-height: 100px;
-            float: left;
-            margin: 0 0 0 0;
-            padding: 0 0 0 0;
-            }
-        </style>
-        <div class="campaignNotesDialog ">
-            <a onClick="game.actors.getName('`+charName+`').sheet.render(true)">
-                <img id="charImgFor`+charName+`"src="`+charImg+`">
-            </a>
-            <br>`+ campaignNotes +`
-        </div>`;
+        let dialogBox = `
+            <style>
+                .campaignNotesDialogGGP{
+                    background-color: rgba(0,0,0,0.5);
+                    background-blend-mode: lighten;
+                    min-width: `+minWidth+`px !important;
+                    max-width: `+maxWidth+`px !important;
+                    max-height:`+maxHeight+`px !important;
+                    overflow-x: auto;
+                    overflow-y: scroll;
+                    width: auto !important;
+                    backdrop-filter: blur(2px);
+                    }
+                    
+                .campaignNotesDialogGGP > .window-header{
+                    background: rgba(0,0,0,0.25);
+                    font-size: 16pt;
+                    height: auto;
+                    }
+                    
+                .campaignNotesDialogGGP > .window-content {
+                    background: unset !important;
+                    }
+                    
+                .campaignNotesDialog {
+                    color:white;
+                    }
+                    
+                #charImgFor`+charName+` {
+                    max-width: 50px;
+                    max-height: 100px;
+                    float: left;
+                    margin: 0 0 0 0;
+                    padding: 0 0 0 0;
+                    }
+                
+            </style>
+            
+            <div class="campaignNotesDialog ">
+            
+                <a onClick="game.actors.getName('`+charName+`').sheet.render(true)">
+                    <img id="charImgFor`+charName+`"src="`+charImg+`">
+                </a>
+                
+                <br>`+ campaignNotes +`
+            </div>
+            `;
         
         //time to spawn the dialog window
         new Dialog({
@@ -149,9 +160,9 @@ for (let key in selectedActors) {
             //unique name for each dialog window using the character's 
             //name
             title: "Notes for " + charName, content: dialogBox, buttons: {}
-            
+            },
             //time to use that positional data from earlier and make it render
-            },dialogBoxOptions).render(true);
+            dialogBoxOptions).render(true);
             
         //Some debugging stuff to double check if an issue arises.
         //console.log(key + ": Opening Campaign Notes " + charName)
